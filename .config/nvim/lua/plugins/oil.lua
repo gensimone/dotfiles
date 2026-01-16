fa2e260556c67e60c17b65d470d1f256335997b4
Simone Gentili <gensimone.git@gmail.com>
2026-01-16 10:18:26 +0100

    Update oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -18,12 +18,12 @@ return {
           show_hidden = true
       },
       -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-      delete_to_trash = true,
+      delete_to_trash = false,
       -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
       skip_confirm_for_simple_edits = true,
       -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
       -- (:help prompt_save_on_select_new_entry)
-      prompt_save_on_select_new_entry = false,
+      prompt_save_on_select_new_entry = true,
       -- Keymaps
       keymaps = {
 		  ["g?"] = { "actions.show_help", mode = "n" },
