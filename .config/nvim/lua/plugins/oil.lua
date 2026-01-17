7a4df1276c829cc0a2fd8644f93ded0d26ba22c9
Simone Gentili <gensimone.git@gmail.com>
2026-01-17 12:12:11 +0100

    Update oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -9,7 +9,7 @@ return {
       -- Id is automatically added at the beginning, and name at the end
       -- See :help oil-columns
 	  columns = {
-		  -- "icon",
+		  "icon",
 		  "permissions",
 		  "size",
 		  "mtime",
@@ -47,7 +47,7 @@ return {
   },
   -- Optional dependencies
   -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
